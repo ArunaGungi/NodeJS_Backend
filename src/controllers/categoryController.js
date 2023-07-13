184907722b97091e814b0a7b8e00105fe3d4a597
AKhilXelp <akhil.anem@xelpmoc.in>
2023-07-13 16:29:07 +0530

    Categories CRUD API's Integrated

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -6,13 +6,13 @@ module.exports = {
     // console.log("category details", req.body);
     // console.log("sequelize models", sequelize.models);
     await sequelize.models.Categories.create({
-      categoryName
+      categoryName,
     })
       .then((category) => {
         res.status(200).send({
           code: 200,
           message: "Category added successfully",
-          data:category 
+          data: category,
         });
       })
       .catch((err) => {
@@ -22,4 +22,69 @@ module.exports = {
         });
       });
   },
+  getAllCategories: async (req, res) => {
+    await sequelize.models.Categories.findAll()
+      .then((categoryData) =>
+        res.status(200).send({
+          code: "200",
+          message: "Category details fetched successfully",
+          data: categoryData,
+          status: "success",
+        })
+      )
+      .catch((err) =>
+        res.status(400).send({
+          code: 400,
+          message: err,
+          status: "failure",
+        })
+      );
+  },
+  updateCategory: async (req, res) => {
+    console.log("request under update category", req.params.id);
+    const id  = req.params.id;
+    console.log("category id value", id);
+    await sequelize.models.Categories.findOne({
+      where: {
+        id: id,
+      },
+    }).then((category) =>
+      category
+        .update(req.body)
+        .then((updatedCategory) => {
+          res.status(200).send({
+            code: 200,
+            message: "Category Details updated successfully",
+            data: updatedCategory,
+          });
+        })
+        .catch((err) =>
+          res.status(400).send({
+            code: 400,
+            message: `Could not update ${err}`,
+          })
+        )
+    );
+  },
+  deleteCategory:async(req,res) => {
+    const id = req.params.id;
+    await sequelize.models.Categories.findOne({
+      where:{
+        id:id
+      }
+    }).then((category) => {
+      category.destroy().then((deletedCategory) => {
+        res.status(200).send({
+          code:200,
+          message:"Deleted Category Successfully",
+          data:deletedCategory
+        })
+      }).catch((err) => {
+        res.status(400).send({
+          code:400,
+          message:err
+        })
+      })
+    })
+  }
 };
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -10,5 +10,14 @@ categoryRouter.use(cors());
 categoryRouter.post("/add", (req,res) => {
     categoryController.addCategory(req,res);
 })
+categoryRouter.get("/getAllCategories", (req,res) => {
+    categoryController.getAllCategories(req,res);
+})
+categoryRouter.put("/updateCategory/:id", (req,res) => {
+    categoryController.updateCategory(req,res);
+})
+categoryRouter.delete("/deleteCategory/:id", (req,res) => {
+    categoryController.deleteCategory(req,res);
+})
 
 module.exports = {categoryRouter}
